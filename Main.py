381c90f6928f3b6ddd0c87149d35416832b2a72d
Yojana Gadiya <45199062+YojanaGadiya@users.noreply.github.com>
2024-10-09 09:08:50 +0200

    update main

--- Main.py
+++ Main.py
@@ -5,23 +5,33 @@ import streamlit as st
 import base64
 from streamlit_card import card
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    layout="wide", 
+    page_title="ITMP Data Science Toolkits", 
+    page_icon="🧊", 
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 
 # Customize sidebar
-markdown = """
-**Info**: This is a collection of data science tools developed by Fraunhofer ITMP SreeningPort.
+# markdown = """
+# **Info**: This is a collection of data science tools developed by Fraunhofer ITMP SreeningPort.
 
-**External links**:
-* [GitHub](https://github.com/Fraunhofer-ITMP/streamlit_ITMP)
-* [Website](https://www.itmp.fraunhofer.de/)
-"""
+# **External links**:
+# * [GitHub](https://github.com/Fraunhofer-ITMP/streamlit_ITMP)
+# * [Website](https://www.itmp.fraunhofer.de/)
+# """
 
-st.sidebar.title("About")
-st.sidebar.markdown(markdown)
-st.sidebar.image("images/fraunhofer_ITMP-logo_900p.jpg")
+# st.sidebar.title("About")
+# st.sidebar.markdown(markdown)
+# st.sidebar.image("images/fraunhofer_ITMP-logo_900p.jpg")
 
 st.markdown(
-    "<h1 style='text-align: center; color: #149372;'>Fraunhofer ITMP ScreeningPort <br> Data Science Toolkits</h1>",
+    "<h1 style='text-align: center; color: #149372;'>Fraunhofer ITMP Data Science Toolkits</h1>",
     unsafe_allow_html=True,
 )
 
@@ -35,13 +45,19 @@ def load_logo(filepath):
     data = "data:image/png;base64," + encoded.decode("utf-8")
     return data
 
+st.markdown("### About")
+st.markdown(
+    "This is a collection of data science tools developed by Fraunhofer ITMP SreeningPort."
+)
 
-# Add cards for each app
-#col1, col2, col3, col4, col5, col6 = st.columns(6)
+tab1, tab2, tab3 = st.tabs(3)
 
-col1, col2  = st.columns(2)
 
-col3, col4  = st.columns(2)
+
+col1, col2, col3  = st.columns(3)
+
+# col3, col4  = st.columns(2)
+
 
 with col1:
     hasClicked = card(
@@ -67,26 +83,20 @@ with col3:
         url="/kgg",
     )
 
-with col4:
-    hasClicked = card(
-        title="KGG_user",
-        text="Test",
-        image=load_logo("images/kg_1.png"),
-        url="/kggUser",
-    )
-
-# with col5:
+# with col4:
 #     hasClicked = card(
-#         title="KGG workflow test 1",
-#         text="Test2Jupyter",
-#         image=load_logo("images/kg_2.png"),
-#         url="/kgg_user_2",
-#     )
-
-# with col6:
-#     hasClicked = card(
-#         title="KGG workflow test 2",
+#         title="KGG_user",
 #         text="Test",
 #         image=load_logo("images/kg_1.png"),
-#         url="/kgg_user_3",
+#         url="/kggUser",
 #     )
+
+
+# footer with text and green background
+st.markdown(
+    "<footer style='background-color: #006c8b; padding: 10px; border-radius: 10px;'>"
+    "<p style='color: white; text-align: center;'>Fraunhofer ITMP © 2021</p>"
+    "<p style='color: white; text-align: center;'>This work has been conducted across several key projects in which ITMP has been actively involved.</p>"
+    "</footer>",
+    unsafe_allow_html=True,
+)
