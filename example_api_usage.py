38e060b07c269904561d8adfbbba9394160c45cb
bishab <bishabpokharel503@gmail.com>
2025-09-16 13:17:01 +0000

    Update example usage

--- example_api_usage.py
+++ example_api_usage.py
@@ -1,12 +1,19 @@
 import requests
 
 # Requesting a Token
-response = requests.post("http://10.164.197.141:8080/request_token", data={"username": "kgg_user1", "password": "gast1@kgg2025"})
+response = requests.post("https://kggapi.serve.scilifelab.se/request_token", data={"username": "kgg_user1", "password": "gast1@kgg2025"})
 token = response.json().get("access_token")
 print(token)
 
-# Making an API call
+
+# Getting a Disease ID
 headers = {"Authorization": f"Bearer {token}"}
-response = requests.post("http://10.164.197.141:8080/createKG", json={"disease_id": "MONDO_0004976", "clinical_trial_phase": 3, "protein_threshold": 0.8}, headers=headers)
+response = requests.post("https://kggapi.serve.scilifelab.se/getDiseaseIDs", json={"disease_name": "cancer"}, headers=headers)
+print(response.json())
+
+
+# Creating a KG
+headers = {"Authorization": f"Bearer {token}"}
+response = requests.post("https://kggapi.serve.scilifelab.se/createKG", json={"disease_id": "MONDO_0004992", "clinical_trial_phase": 3, "protein_threshold": 0.8}, headers=headers)
 print(response.json())
 
